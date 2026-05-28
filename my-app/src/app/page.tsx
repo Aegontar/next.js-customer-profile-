690fb3aca52a3131a9421e5f99e10155949ccac5
aegontar <charlesdessa1@hotmail.com>
2026-05-28 17:44:44 +0600

    log in file added

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.header}>Keep Track of all Customers</main>
+    </div>
+  );
+}
---
+++ my-app/src/app/ui/log-in-form.tsx
@@ -0,0 +1,13 @@
+const logIn = () => {
+  return (
+    <div className="log-in-form">
+      <div className="form">
+        <h1>Welcome Back!</h1>
+        <p>Welcome back! Please enter your details!</p>
+      </div>
+      <div className="graphic"></div>
+    </div>
+  );
+};
+
+export default logIn;
