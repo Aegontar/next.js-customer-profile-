10f3f62d3c1d828f9b96c102a0e9d9df6abd27f9
aegontar <charlesdessa1@hotmail.com>
2026-06-14 07:29:45 +0600

    added favicon and other pages

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { inter } from "@/app/ui/fonts";
 
 export const metadata: Metadata = {
   title: "Home page",
@@ -23,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,16 @@
 import styles from "./page.module.css";
+import Form from "next/form";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.header}>Keep Track of all Customers</main>
+      <Form action="/search">
+        {/* On submission, the input value will be appended to
+          the URL, e.g. /search?query=abc */}
+        <input name="query" />
+        <button type="submit">Submit</button>
+      </Form>
     </div>
   );
 }
